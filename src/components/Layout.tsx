7be01c33767bda1d87527442b94c8ffa77b885e0
Jonathan Haas <jonathan@haas.holdings>
2025-04-12 13:56:53 -0700

    feat: only show reading progress bar on blog pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { ReadingProgressBar } from './ReadingProgressBar';
@@ -8,9 +9,12 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const location = useLocation();
+  const isBlogPage = location.pathname.startsWith('/blog');
+
   return (
     <div className="flex min-h-screen flex-col bg-background text-foreground">
-      <ReadingProgressBar />
+      {isBlogPage && <ReadingProgressBar />}
       <Navbar />
       <main className="flex-1">
         {children}
